9930841adb6dc134c425ca1365b3a2ffe0328026
daipresents <daipresents@gmail.com>
2016-05-09 11:29:21 +0900

    add license note

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-/*
+/*  Copyright 2016 Dai Fujihara  (email : daipresents[at]gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+?>
+<?php
 Plugin Name: Related Affiliate Item by Rakuten and Amazon (RAIRA)
 Plugin URI: http://daipresents.com/2016/wp-related-affiliate-item-by-rakuten-amazon-plugin/
 Description: This plugin support you to monetize your blog by displaying the contents which are affiliate image, link etc.
